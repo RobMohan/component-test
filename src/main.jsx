0da86d695697bbe7617a080008a4f62c4c6415d1
Rob M <robmohan334@gmail.com>
2025-10-17 18:17:15 -0400

    removing “s” on global style file
    There was an extra “s” on global
    
    Signed-off-by: Rob M <robmohan334@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ButtonDemo from './examples/ButtonDemo';
-import './styles/globals.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
