32cd5827d9ff2a4a6cd80341b77d6a856f53b899
Rob M <robmohan334@gmail.com>
2025-10-18 22:28:38 -0400

    Change imports
    refelcting the file updates in main
    
    Signed-off-by: Rob M <robmohan334@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ButtonDemo from './examples/ButtonDemo';
-import './styles/global.css';
+import DesignSystemDemo from './examples/DesignSystemDemo';  // Changed this
+import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ButtonDemo />
+    <DesignSystemDemo />
   </React.StrictMode>
 );
